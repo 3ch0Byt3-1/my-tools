1784d6b3c7c4fa185a32cdd69dafd3db5e844a86
Safiqul Islam Tarun <iamtarun.bd@gmail.com>
2024-11-17 17:31:19 +0600

    Create sub.sh

---
+++ sub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Tarun
+# Date: 2024-11-17
+# Description: This script fetches subdomains of a given domain using the SecurityTrails API and appends the domain name.#------------------------------------------------------------------------------------------------------------------------# Prompt for the domain name
+read -p "Enter the domain (e.g., apple.com): " domain
+#--------------------------------------------------------------------
+curl "https://api.securitytrails.com/v1/domain/$domain/subdomains" \
+-H "apikey: sY2Cq8MTA4BwN7diurC6VM5fpwjyG1DL" | \
+jq -r ".subdomains[] + \".$domain\""
