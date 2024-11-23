5b8bdda55566b1649604bda0729a99df4aff59ac
Safiqul Islam Tarun <iamtarun.bd@gmail.com>
2024-11-23 13:59:11 +0600

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#--------------------------------------------------------------------
 # Author: Tarun
 # Date: 2024-11-17
 # Description: This script fetches subdomains of a given domain using the SecurityTrails API and appends the domain name.#------------------------------------------------------------------------------------------------------------------------# Prompt for the domain name
+#--------------------------------------------------------------------
 read -p "Enter the domain (e.g., apple.com): " domain
 #--------------------------------------------------------------------
 curl "https://api.securitytrails.com/v1/domain/$domain/subdomains" \
