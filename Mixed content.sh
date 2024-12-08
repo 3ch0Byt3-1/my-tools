52c99eeb5e06493ab0a9f844731d3f21ec9206d4
Safiqul Islam Tarun <iamtarun.bd@gmail.com>
2024-12-08 11:56:22 +0600

    Update Mixed content.sh

--- Mixed content.sh
+++ Mixed content.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 read -p "Enter the HTTPS website URL (e.g., https://example.com): " url
+
 echo "Scanning $url for mixed content..."
+
 resources=$(curl -s "$url" | grep -Eo 'http[s]?://[^"]+')
+
 mixed_content=false
 while IFS= read -r resource; do
     if [[ "$resource" == http://* ]]; then
