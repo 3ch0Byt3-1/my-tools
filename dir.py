01cb6ffb566a1d2b7c2a24b394ab612914169d35
Safiqul Islam Tarun <iamtarun.bd@gmail.com>
2024-12-10 21:11:00 +0600

    Update dir.py

--- dir.py
+++ dir.py
@@ -1,10 +1,14 @@
 import requests
+#--------------------------------------------------------------------
 base_url = "https://bytecapsule.io"
+#--------------------------------------------------------------------
 paths = [
     "cgi-bin", "assets", "webmail", "controlpanel", "cpanel", "whm", 
     "~mike", "~jeff", "~masmith", "~rachna", "~greg"
 ]
+#--------------------------------------------------------------------
 for path in paths:
+#--------------------------------------------------------------------
     url = f"{base_url}/{path}"
     try:
         response = requests.get(url, allow_redirects=False)
@@ -13,5 +17,6 @@ for path in paths:
             print(f"Content Snippet: {response.text[:200]}\n")
         elif response.status_code in [301, 302]:
             print(f"Redirects To: {response.headers.get('Location')}\n")
+#--------------------------------------------------------------------
     except requests.exceptions.RequestException as e:
         print(f"Error accessing {url}: {e}")
